a8ae0c25c223d4b92afcb017edda5eda9bd5ee85
brintasabit <brintasabit@gmail.com>
2019-09-16 18:39:24 +0600

    Print not working if index++

--- Assignment_3_bitm_seip/Assignment_3_bitm_seip/HomeCoffeeShop.cs
+++ Assignment_3_bitm_seip/Assignment_3_bitm_seip/HomeCoffeeShop.cs
@@ -12,10 +12,16 @@ namespace Assignment_3_bitm_seip
 {
     public partial class HomeCoffeeShop : Form
     {
-        const int size = 5;
-        //int index = 0;
-        string[] array = new string[size];
-        int[] array2 = new int[size];
+        const int size = 2;
+        int index = 0;
+      //  string[] array = new string[size];
+        //string[] array2 = new string[size];
+        string[] customerName = new string[size];
+        string[] contactNumber = new string[size];
+        string[] address = new string[size];
+        string[] order = new string[size];
+        
+        int[] quantity = new int[size];
         public HomeCoffeeShop()
         {
             InitializeComponent();
@@ -23,35 +29,62 @@ namespace Assignment_3_bitm_seip
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            array[0] = customerNameTextBox.Text;
+            int totalPrice=0;
+            /*array[0] = customerNameTextBox.Text;
             array[1] = contactNumberTextBox.Text;
             array[2] = AddressTextBox.Text;
             array[3] = orderComboBox.Text;
-            array2[0] = Convert.ToInt32(quantityTextBox.Text);
-            int totalPrice = 0;
-            if (array[3] == "Black")
+            array2[0] = Convert.ToInt32(quantityTextBox.Text);*/
+            if (index < size)
             {
-                 totalPrice = array2[0] * 120;
+                customerName[index] = customerNameTextBox.Text;
+                contactNumber[index] = contactNumberTextBox.Text;
+                address[index] = AddressTextBox.Text;
+                order[index] = orderComboBox.Text;
+                quantity[index] = Convert.ToInt32(quantityTextBox.Text);
+                index++;
             }
-            else if (array[3] == "Cold")
+            
+            if (order[index] == "Black")
             {
-                totalPrice = array2[0] * 100;
+                totalPrice = quantity[index] * 120;
             }
-            else if (array[3] == "Hot")
+            else if (order[index] == "Cold")
             {
-                totalPrice = array2[0] * 90;
+                totalPrice = quantity[index] * 100;
             }
-            else if (array[3] == "Regular")
+            else if (order[index] == "Hot")
             {
-                totalPrice = array2[0] * 80;
+                totalPrice = quantity[index] * 90;
             }
-            else
+            else if (order[index] == "Regular")
             {
-                MessageBox.Show("Please Select An Item");
+                totalPrice = quantity[index] * 80;
             }
-            purchaseInformationRichTextBox.AppendText("\n\nName: " + array[0] + "\nContact: " 
-                + array[1] + "\nAddress: " + array[2] + "\nOrder: " + array[3] + "\nQuantity: " 
-                + array2[0]+"\nTotal Price " +totalPrice);
+            /* if (array[3] == "Black")
+             {
+                  totalPrice = array2[0] * 120;
+             }
+             else if (array[3] == "Cold")
+             {
+                 totalPrice = array2[0] * 100;
+             }
+             else if (array[3] == "Hot")
+             {
+                 totalPrice = array2[0] * 90;
+             }
+             else if (array[3] == "Regular")
+             {
+                 totalPrice = array2[0] * 80;
+             }
+             else
+             {
+                 MessageBox.Show("Please Select An Item");
+             }*/
+            purchaseInformationRichTextBox.AppendText("\n\nName: " + customerName[index] 
+                + "\nContact: " + contactNumber[index] + "\nAddress: " + address[index] 
+                + "\nOrder: " + order[index] + "\nQuantity: "+ quantity[index]
+                +"\nTotal Price " +totalPrice);
             customerNameTextBox.Text = " ";
             contactNumberTextBox.Text = " ";
             AddressTextBox.Text = " ";
